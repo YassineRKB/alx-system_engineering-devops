f305fc88802ae92e92232224572dab404d3a217d
Yassine Rakibi <yassine@arcraven.com>
2023-09-24 23:44:13 +0200

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
-# setting a server coniguration using puppet manifest
-file_line {'alxServConf'
+# setting a server configuration using puppet manifest
+file_line {'alxServConf':
   path => '/etc/ssh/ssh_config',
   line => 'Host 35.153.16.204
-  	HostName 35.153.16.204
-	User ubuntu
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no',
+    HostName 35.153.16.204
+    User ubuntu
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no',
 }
