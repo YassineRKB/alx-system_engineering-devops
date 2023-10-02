5250e035d72f2eff486e19736efcb46a20f6a0e8
Yassine Rakibi <yassine@arcraven.com>
2023-10-03 01:18:43 +0200

    task 2: v0.3

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,13 +1,13 @@
 # puppet manifest to config a server
 exec { 'apt operations':
-  command     => 'sudo apt-get -y update && sudo apt-get -y install nginx',
+  command     => 'sudo apt-get -y update; sudo apt-get -y install nginx',
   provider => shell,
 }
-exec { 'seeding the configuration':
+-> exec { 'seeding the configuration':
   command => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
   provider => shell,
 }
-exec { 'service nginx restart':
+-> exec { 'service nginx restart':
   command => 'sudo service nginx restart',
   provider => shell,
 }
