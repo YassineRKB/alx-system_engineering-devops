e8fc7244da66361d6baa2e9f202c9d2ac0f86011
YassineRKB <yassine@arcraven.com>
2023-10-23 21:37:09 +0200

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Python script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODO list progress.
+export into JSON format"""
+
+from json import dump
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    domain = 'https://jsonplaceholder.typicode.com'
+    param = argv[1]
+    todo = domain + f"/user/{param}/todos"
+    user = domain + f"/users/{param}"
+    todoJSON = requests.get(todo).json()
+    userJSON = requests.get(user).json()
+    JSONROWDATA = []
+    JSONDATA = {}
+    with open(f"{param}.json", mode="w") as stream:
+        for taskdone in todoJSON:
+            row = {
+                "task": taskdone.get('title'),
+                "completed": taskdone.get("completed"),
+                "username": userJSON.get("username")
+            }
+            JSONROWDATA.append(row)
+        JSONDATA[param] = JSONROWDATA
+        dump(JSONROWDATA, stream)
