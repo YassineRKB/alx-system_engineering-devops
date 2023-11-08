e5644bec3c60d82720f64583826afe6c9e1c9cea
YassineRKB <yassine@arcraven.com>
2023-11-08 12:24:22 +0100

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""get top ten posts of a subreddit passed as argument"""
+import requests
+
+
+def top_ten(subreddit):
+    """Print None if invalid or top ten titles for a valid subreddit"""
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'mom!'})
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    req = requests.get(
+        url,
+        params={"limit": 10},
+        headers=headers
+        ).json()
+    posts = req.get(
+        'data',
+        {}
+        ).get('children', [])
+    if not posts:
+        print(None)
+    for title in posts:
+        print(title.get('data').get('title'))
